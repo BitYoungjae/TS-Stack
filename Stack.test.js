b9caa0e5f8d272b2d9b4634f1e32a8b9cb8d70c7
BitYoungjae <bityoungjae@gmail.com>
2020-04-04 20:57:16 +0900

    Add Test Cases : New Members isEmpty clear top

--- Stack.test.js
+++ Stack.test.js
@@ -26,3 +26,36 @@ test('New Stack with 0 size', () => {
     const stack = new Stack(0);
   }).toThrow();
 });
+
+describe('New Features', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack(3);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+  });
+
+  it('isEmpty', () => {
+    expect(stack.isEmpty).toBe(false);
+
+    const emptyStack = new Stack(3);
+    expect(emptyStack.isEmpty).toBe(true);
+  });
+
+  it('clear', () => {
+    stack.clear();
+    expect(stack.isEmpty).toBe(true);
+
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+  });
+
+  it('top', () => {
+    expect(stack.top).toBe(3);
+  });
+});
